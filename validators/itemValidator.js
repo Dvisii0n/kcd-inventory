361d15d8fb8c85d492020aeea1f76ddd66c23fb8
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-31 21:48:49 -0600

    Refactored item validation middleware to use it twice, one in create and one in update where all fields are optional except the req id param

--- validators/itemValidator.js
+++ validators/itemValidator.js
@@ -1,4 +1,4 @@
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 
 const isIntError = (min, max) =>
 	`should be an integer between ${min} and ${max}`;
@@ -10,43 +10,63 @@ const isAplhaError = "should only contain letters";
 
 const isFloatError = "should be a decimal number and no less than 0";
 
-const validateItem = [
-	body("name")
+const itemValidationRules = {
+	name: body("name")
 		.isAlpha("en-US", { ignore: " " })
 		.withMessage(`Name ${isAplhaError}`)
 		.isLength({ min: 3, max: 25 })
 		.withMessage(`Name ${lengthError(3, 25)} `),
 
-	body("quantity")
+	quantity: body("quantity")
 		.trim()
 		.isInt({ min: 1, max: 50 })
 		.withMessage(`Quantity ${isIntError(1, 50)}`),
 
-	body("power")
+	power: body("power")
 		.isInt({ min: 0 })
 		.withMessage(`Power should be higher than 0`),
 
-	body("quality")
+	quality: body("quality")
 		.trim()
 		.isInt({ min: 0, max: 100 })
 		.withMessage("Quality should be no less than 0 and no higher than 100"),
 
-	body("weight")
+	weight: body("weight")
 		.trim()
 		.isFloat({ min: 0 })
 		.withMessage(`Weight ${isFloatError}`),
 
-	body("value")
+	value: body("value")
 		.trim()
 		.isFloat({ min: 0 })
 		.withMessage(`Value ${isFloatError}`),
 
-	body("categoryId")
+	categoryId: body("category_id")
 		.trim()
 		.isInt()
 		.withMessage("CategoryId should be an integer")
 		.isInt({ min: 1 })
 		.withMessage(`CategoryId should be higher than 0`),
+};
+
+const validateCreateItem = [
+	itemValidationRules.name,
+	itemValidationRules.quantity,
+	itemValidationRules.power,
+	itemValidationRules.quality,
+	itemValidationRules.weight,
+	itemValidationRules.value,
+	itemValidationRules.categoryId,
+];
+const validateUpdateItem = [
+	param("id").exists().isInt({ min: 1 }).withMessage("Nice try idiot"),
+	itemValidationRules.name.optional(),
+	itemValidationRules.quantity.optional(),
+	itemValidationRules.power.optional(),
+	itemValidationRules.quality.optional(),
+	itemValidationRules.weight.optional(),
+	itemValidationRules.value.optional(),
+	itemValidationRules.categoryId.optional(),
 ];
 
-export default validateItem;
+export default { validateCreateItem, validateUpdateItem };
