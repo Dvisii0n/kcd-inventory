1c1a02fff4a8b5ea36afb1e90516ad8a71ae21b4
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-31 21:47:11 -0600

    Added dynamic update item query

--- db/queries.js
+++ db/queries.js
@@ -15,4 +15,18 @@ async function createItem(itemData) {
 		[name, quantity, power, quality, weight, value, categoryId],
 	);
 }
-export default { getItems, createItem };
+
+async function updateItem(itemDataWithId) {
+	const valuesArray = Object.values(itemDataWithId);
+	let i = 2;
+	let fields = [];
+	for (let key of Object.keys(itemDataWithId)) {
+		if (key === "id") continue;
+		fields.push(`${key} = $${i}`);
+		i++;
+	}
+
+	const query = `UPDATE items SET ${fields.join(", ")} WHERE id = $1`;
+	await pool.query(query, valuesArray);
+}
+export default { getItems, createItem, updateItem };
