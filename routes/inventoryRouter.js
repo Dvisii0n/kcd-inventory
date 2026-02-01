f9c6e0ede6a9b7115136821e1ca97ec5976a7531
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-31 21:47:44 -0600

    Added update item route

--- routes/inventoryRouter.js
+++ routes/inventoryRouter.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import invController from "../controllers/inventoryController.js";
-import validateItem from "../validators/itemValidator.js";
+import itemValidator from "../validators/itemValidator.js";
 
 const inventoryRouter = new Router();
 
 inventoryRouter.get("/", invController.getInventory);
-inventoryRouter.post("/create", validateItem, invController.createItem);
+inventoryRouter.post(
+	"/create",
+	itemValidator.validateCreateItem,
+	invController.createItem,
+);
+
+inventoryRouter.put(
+	"/update/:id",
+	itemValidator.validateUpdateItem,
+	invController.updateItem,
+);
 
 export default inventoryRouter;
