267353b0c3c17e660b7766ccebd59fcdfad4c1fb
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-02-04 19:28:22 -0600

    Added categories controller

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,8 +1,57 @@
 import categoriesQueries from "../db/categoriesQueries.js";
+import { matchedData } from "express-validator";
+
+const categoriesURL = "/inventory/categories";
+
 async function getCategories(req, res) {
 	try {
 		const categories = await categoriesQueries.getCategories();
-		res.render("categories", { title: "Categories", categories: categories });
+		res.render("categories", {
+			title: "Categories",
+			categories: categories,
+		});
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function getCategoryItems(req, res) {
+	try {
+		const { categoryName } = matchedData(req);
+		const categoryItems =
+			await categoriesQueries.getCategoryItems(categoryName);
+		res.render("inventory", { title: "Inventory", items: categoryItems });
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function createCategory(req, res) {
+	try {
+		const { categoryName } = matchedData(req);
+		await categoriesQueries.createCategory(categoryName);
+		res.redirect(categoriesURL);
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function updateCategory(req, res) {
+	try {
+		const { categoryId, categoryName } = matchedData(req);
+		console.log(matchedData(req));
+		await categoriesQueries.updateCategory(categoryId, categoryName);
+		res.redirect(categoriesURL);
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function deleteCategory(req, res) {
+	try {
+		const { categoryId } = matchedData(req);
+		await categoriesQueries.deleteCategory(categoryId);
+		res.redirect(categoriesURL);
 	} catch (error) {
 		throw error;
 	}
@@ -10,4 +59,8 @@ async function getCategories(req, res) {
 
 export default {
 	getCategories,
+	getCategoryItems,
+	createCategory,
+	updateCategory,
+	deleteCategory,
 };
