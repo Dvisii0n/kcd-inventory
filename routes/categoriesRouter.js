bf0053f8b541025ec01205506c4072d60445e1dc
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-02-04 19:30:58 -0600

    Added routes with their respected validation middleware and controllers

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,8 +1,38 @@
 import { Router } from "express";
 import catController from "../controllers/categoriesController.js";
+import catValidator from "../validators/categoriesValidator.js";
+import { validationErrorHandler } from "../validators/validationUtils.js";
 
 const categoriesRouter = new Router();
 
 categoriesRouter.get("/", catController.getCategories);
+
+categoriesRouter.get(
+	"/items/:categoryName",
+	catValidator.validateCategoryParam,
+	validationErrorHandler,
+	catController.getCategoryItems,
+);
+
+categoriesRouter.post(
+	"/create",
+	catValidator.validateCategoryName,
+	validationErrorHandler,
+	catController.createCategory,
+);
+
+categoriesRouter.put(
+	"/update/:categoryId",
+	catValidator.validateCategoryUpdate,
+	validationErrorHandler,
+	catController.updateCategory,
+);
+
+categoriesRouter.delete(
+	"/delete/:categoryId",
+	catValidator.validateCategoryId,
+	validationErrorHandler,
+	catController.deleteCategory,
+);
 
 export default categoriesRouter;
