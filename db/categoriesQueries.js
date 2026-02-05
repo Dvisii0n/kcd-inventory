496150a4cc6c860c676ccd2214286404a5e43f64
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-02-04 19:29:32 -0600

    Added all categories CRUD queries

--- db/categoriesQueries.js
+++ db/categoriesQueries.js
@@ -5,4 +5,40 @@ async function getCategories() {
 	return rows;
 }
 
-export default { getCategories };
+async function getCategoryItems(categoryName) {
+	const { rows } = await pool.query(
+		`
+         SELECT items.id, items.name, quantity, power, quality, weight, value, categories.name AS category
+		 FROM items 
+         JOIN categories ON items.category_id = categories.id 
+         WHERE categories.name = $1
+		`,
+		[categoryName],
+	);
+	return rows;
+}
+
+async function createCategory(categoryName) {
+	await pool.query("INSERT INTO categories (name) VALUES ($1)", [
+		categoryName,
+	]);
+}
+
+async function updateCategory(categoryId, newCategoryName) {
+	await pool.query("UPDATE categories SET name = $2 WHERE id = $1", [
+		categoryId,
+		newCategoryName,
+	]);
+}
+
+async function deleteCategory(categoryId) {
+	await pool.query("DELETE FROM categories WHERE id = $1", [categoryId]);
+}
+
+export default {
+	getCategories,
+	getCategoryItems,
+	createCategory,
+	updateCategory,
+	deleteCategory,
+};
