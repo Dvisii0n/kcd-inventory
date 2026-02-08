552f7fea8e2a6a70c17051e661f59dd2d94ba68d
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-02-07 18:33:33 -0600

    added different validation functions for each form

--- validators/validationUtils.js
+++ validators/validationUtils.js
@@ -1,4 +1,6 @@
-import { validationResult } from "express-validator";
+import { matchedData, validationResult } from "express-validator";
+import categoriesQueries from "../db/categoriesQueries.js";
+import itemQueries from "../db/itemQueries.js";
 
 async function validationErrorHandler(req, res, next) {
 	const errors = validationResult(req);
@@ -10,4 +12,38 @@ async function validationErrorHandler(req, res, next) {
 	}
 }
 
-export { validationErrorHandler };
+async function createFormValidationHandler(req, res, next) {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const categories = await categoriesQueries.getCategories();
+		res
+			.status(400)
+			.render("createForm", { categories: categories, errors: errors.array() });
+		return;
+	} else {
+		next();
+	}
+}
+
+async function updateFormValidationHandler(req, res, next) {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const { itemId } = matchedData(req);
+		const categories = await categoriesQueries.getCategories();
+		const [item] = await itemQueries.getItem(itemId);
+		res.status(400).render("updateForm", {
+			item: item,
+			categories: categories,
+			currentCategoryId: item["category_id"],
+			errors: errors.array(),
+		});
+		return;
+	} else {
+		next();
+	}
+}
+export {
+	validationErrorHandler,
+	createFormValidationHandler,
+	updateFormValidationHandler,
+};
